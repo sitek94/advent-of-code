309c2527c4ac104e1caca25172bd54d50f28fa94
Maciek <msitkowski94@gmail.com>
2021-12-07 08:29:32 +0100

    day 7 wip

---
+++ src/7/3.ts
@@ -0,0 +1,19 @@
+// ONLY HORIZONTALLY
+
+import { abs, max, min } from '../utils';
+
+import { realInput } from './input';
+
+// const input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14].sort((a, b) => a - b);
+const input = realInput.sort((a, b) => a - b);
+
+// console.log(input);
+
+const M = {};
+console.log(input);
+let median = input[input.length / 2];
+let ans = 0;
+for (const x of input) {
+  ans += abs(x - median);
+}
+console.log(ans);
