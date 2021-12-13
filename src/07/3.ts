6a3f22f204e1a17f4d9961eb8ee00c84cbef2e35
Maciek <msitkowski94@gmail.com>
2021-12-13 21:22:50 +0100

    pad zero day directories
    closes #1

--- src/07/3.ts
+++ src/07/3.ts
@@ -1,13 +1,8 @@
-// ONLY HORIZONTALLY
-
-import { abs, max, min } from '../../utils';
+import { abs } from '../../utils';
 
 import { realInput } from './input';
 
-// const input = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14].sort((a, b) => a - b);
 const input = realInput.sort((a, b) => a - b);
-
-// console.log(input);
 
 const M = {};
 console.log(input);
