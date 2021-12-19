5802745f26b56ba08a77c5398e116c175cf89efb
Maciek <msitkowski94@gmail.com>
2021-12-19 07:18:09 +0100

    add day 2

---
+++ src/2020/02/1.ts
@@ -0,0 +1,35 @@
+import { run } from '../../runner';
+
+function solve(input: string) {
+  let lines = input.split('\n');
+
+  let validCount = 0;
+
+  for (let line of lines) {
+    let [rule, password] = line.split(': ');
+    let [counts, letter] = rule.split(' ');
+    let [min, max] = counts.split('-').map(Number);
+
+    let count = password.split('').filter(x => x === letter).length;
+    if (count >= min && count <= max) {
+      validCount++;
+    }
+  }
+
+  return validCount;
+}
+
+run({
+  solve,
+  tests: [
+    {
+      input: `
+        1-3 a: abcde
+        1-3 b: cdefg
+        2-9 c: ccccccccc
+      `,
+      expected: 2,
+    },
+  ],
+  // onlyTests: true,
+});
---
+++ src/2020/02/2.ts
@@ -0,0 +1,44 @@
+import { run } from '../../runner';
+
+function solve(input: string) {
+  let lines = input.split('\n');
+
+  let validCount = 0;
+
+  for (let line of lines) {
+    let [rule, password] = line.split(': ');
+    let [positions, letter] = rule.split(' ');
+    let [pos1, pos2] = positions.split('-').map(Number);
+
+    let count = password
+      .split('')
+      .reduce(
+        (count, c, i) =>
+          c === letter && (i + 1 === pos1 || i + 1 === pos2)
+            ? count + 1
+            : count,
+        0,
+      );
+
+    if (count === 1) {
+      validCount++;
+    }
+  }
+
+  return validCount;
+}
+
+run({
+  solve,
+  tests: [
+    {
+      input: `
+        1-3 a: abcde
+        1-3 b: cdefg
+        2-9 c: ccccccccc
+      `,
+      expected: 1,
+    },
+  ],
+  // onlyTests: true,
+});
