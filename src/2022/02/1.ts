2b58e06d49452c4fb8bf4d61727272d90e673125
Maciek <msitkowski94@gmail.com>
2022-12-02 14:33:24 +0100

    day 2

---
+++ src/2022/02/1.ts
@@ -0,0 +1,58 @@
+import { run } from '../../runner'
+
+function solve(input: string) {
+  let rounds = input.split('\n')
+
+  const ROCK = 'A'
+  const PAPER = 'B'
+  const SCISSORS = 'C'
+
+  let meToOpponentMap = {
+    X: ROCK,
+    Y: PAPER,
+    Z: SCISSORS,
+  }
+
+  let points = {
+    [ROCK]: 1,
+    [PAPER]: 2,
+    [SCISSORS]: 3,
+  }
+
+  let score = 0
+
+  for (let round of rounds) {
+    let [opponent, me] = round.split(' ')
+
+    me = meToOpponentMap[me]
+
+    score += points[me]
+
+    const draw = opponent === me
+    const won =
+      (me === ROCK && opponent === SCISSORS) ||
+      (me === PAPER && opponent === ROCK) ||
+      (me === SCISSORS && opponent === PAPER)
+
+    if (draw) {
+      score += 3
+    } else if (won) {
+      score += 6
+    }
+  }
+
+  return score
+}
+
+run({
+  solve,
+  tests: [
+    {
+      input: `A Y
+B X
+C Z`,
+      expected: 15,
+    },
+  ],
+  // onlyTests: true,
+})
---
+++ src/2022/02/2.ts
@@ -0,0 +1,66 @@
+import { run } from '../../runner'
+
+function solve(input: string) {
+  let rounds = input.split('\n')
+
+  const ROCK = 'A'
+  const PAPER = 'B'
+  const SCISSORS = 'C'
+
+  const NEED_TO_LOOSE = 'X'
+
+  const NEED_TO_WIN = 'Z'
+
+  let points = {
+    [ROCK]: 1,
+    [PAPER]: 2,
+    [SCISSORS]: 3,
+  }
+
+  let winMap = {
+    [ROCK]: PAPER,
+    [PAPER]: SCISSORS,
+    [SCISSORS]: ROCK,
+  }
+
+  let looseMap = {
+    [ROCK]: SCISSORS,
+    [PAPER]: ROCK,
+    [SCISSORS]: PAPER,
+  }
+
+  let score = 0
+
+  for (let round of rounds) {
+    let [opponent, instruction] = round.split(' ')
+
+    let me
+
+    if (instruction === NEED_TO_LOOSE) {
+      me = looseMap[opponent]
+    } else if (instruction === NEED_TO_WIN) {
+      me = winMap[opponent]
+      score += 6
+    } else {
+      me = opponent
+      score += 3
+    }
+
+    score += points[me]
+  }
+
+  return score
+}
+
+run({
+  solve,
+  tests: [
+    {
+      input: `A Y
+B X
+C Z`,
+      expected: 12,
+    },
+  ],
+  //   onlyTests: true,
+})
