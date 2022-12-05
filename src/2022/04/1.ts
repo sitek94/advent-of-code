c3ddb1cc1ce10078535c604f25cfcd54f6b531b5
Maciek <msitkowski94@gmail.com>
2022-12-05 09:49:48 +0100

    day 4

---
+++ src/2022/04/1.ts
@@ -0,0 +1,38 @@
+import { run } from '../../runner'
+
+function solve(input: string) {
+  let pairs = input.split('\n').map(p => p.split(','))
+
+  let count = 0
+
+  // If a contains b
+  const contains = (a1: number, a2: number, b1: number, b2: number) =>
+    (a1 <= b1 && a2 >= b2) || (b1 <= a1 && b2 >= a2)
+
+  for (let [a, b] of pairs) {
+    let [a1, a2] = a.split('-').map(Number)
+    let [b1, b2] = b.split('-').map(Number)
+
+    if (contains(a1, a2, b1, b2)) {
+      count++
+    }
+  }
+
+  return count
+}
+
+run({
+  solve,
+  tests: [
+    {
+      input: `2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8`,
+      expected: 2,
+    },
+  ],
+  // onlyTests: true,
+})
