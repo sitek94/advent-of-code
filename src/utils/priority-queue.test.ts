f6b08fc23e9029bcae793cc64ca85577303feaad
Maciek Sitkowski <msitkowski94@gmail.com>
2022-12-18 12:09:18 +0100

    implement simple priority queue

---
+++ src/utils/priority-queue.test.ts
@@ -0,0 +1,33 @@
+import { PriorityQueue } from './priority-queue'
+
+describe(`${PriorityQueue.name}`, () => {
+  it('enqueues items in correct order with default `compare` function', () => {
+    const pq = new PriorityQueue()
+
+    pq.enqueue(1)
+    pq.enqueue(5)
+    pq.enqueue(3)
+    pq.enqueue(7)
+
+    expect(pq.copy()).toEqual([1, 3, 5, 7])
+  })
+
+  it('enqueues items using custom compare function', () => {
+    type Item = {
+      priority: number
+    }
+    const pq = new PriorityQueue((a: Item, b: Item) => a.priority < b.priority)
+
+    pq.enqueue({ priority: 1 })
+    pq.enqueue({ priority: 5 })
+    pq.enqueue({ priority: 3 })
+    pq.enqueue({ priority: 7 })
+
+    expect(pq.copy()).toEqual([
+      { priority: 1 },
+      { priority: 3 },
+      { priority: 5 },
+      { priority: 7 },
+    ])
+  })
+})
---
+++ src/utils/priority-queue.ts
@@ -0,0 +1,36 @@
+export class PriorityQueue<T> {
+  private collection: T[] = []
+
+  constructor(private compare: (a: T, b: T) => boolean = (a, b) => a < b) {}
+
+  enqueue(item: T) {
+    let added = false
+    for (let i = 0; i < this.size(); i++) {
+      if (this.compare(item, this.collection[i])) {
+        added = true
+        this.collection.splice(i, 0, item)
+        return item
+      }
+    }
+
+    if (!added) {
+      return this.collection.push(item)
+    }
+  }
+
+  dequeue() {
+    return this.collection.shift()
+  }
+
+  size() {
+    return this.collection.length
+  }
+
+  isEmpty() {
+    return this.collection.length === 0
+  }
+
+  copy() {
+    return this.collection.slice()
+  }
+}
