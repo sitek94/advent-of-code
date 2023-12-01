506550973d2ef02e40eaa29f1be2015dccc782e4
Maciek Sitkowski <msitkowski94@gmail.com>
2023-12-01 08:43:45 +0100

    day 1

---
+++ src/2023/01/2.ts
@@ -0,0 +1,51 @@
+import { run } from '../../runner'
+
+function solve(input: string) {
+  let total = 0
+
+  const WORDS = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ]
+
+  input.split('\n').forEach(line => {
+    const digits = [] as string[]
+
+    line.split('').forEach((c, i) => {
+      if (isDigit(c)) digits.push(c)
+
+      let subline = line.slice(i)
+
+      WORDS.forEach((word, wordIndex) => {
+        if (subline.startsWith(word)) digits.push(String(wordIndex))
+      })
+    })
+
+    total += +(digits.at(0) + digits.at(-1))
+  })
+
+  return total
+}
+
+run({
+  solve,
+  tests: [
+    {
+      expected: 281,
+    },
+  ],
+  onlyTests: false,
+})
+
+function isDigit(char: string) {
+  const n = +char
+  return !isNaN(n)
+}
