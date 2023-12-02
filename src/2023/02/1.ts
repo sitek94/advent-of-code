11e7ffd28a8e5d387e63974c06c1e7b373ebda30
Maciek Sitkowski <msitkowski94@gmail.com>
2023-12-02 08:27:24 +0100

    day 2

---
+++ src/2023/02/1.ts
@@ -0,0 +1,45 @@
+import { run } from '../../runner'
+
+function solve(input: string) {
+  let score = 0
+
+  input.split('\n').forEach(line => {
+    const id = +line.split('Game ').pop().split(':')[0]
+    const sets = line.split(': ')[1].split('; ')
+
+    let isPossible = true
+
+    for (const set of sets) {
+      if (!isPossible) {
+        continue
+      }
+
+      const values = { blue: 0, red: 0, green: 0 }
+
+      set.split(', ').flatMap(cube => {
+        const [value, color] = cube.split(' ')
+
+        values[color] += Number(value)
+      })
+
+      isPossible = values.red <= 12 && values.green <= 13 && values.blue <= 14
+    }
+
+    if (isPossible) {
+      score += id
+    }
+  })
+
+  return score
+}
+
+run({
+  solve,
+  tests: [
+    {
+      input: ``,
+      expected: 8,
+    },
+  ],
+  onlyTests: false,
+})
---
+++ src/2023/02/2.ts
@@ -0,0 +1,43 @@
+import { run } from '../../runner'
+
+function solve(input: string) {
+  let score = 0
+
+  input.split('\n').forEach(line => {
+    const sets = line.split(': ')[1].split('; ')
+
+    let minGreen = -Infinity
+    let minRed = -Infinity
+    let minBlue = -Infinity
+
+    for (const set of sets) {
+      const v = { blue: 0, red: 0, green: 0 }
+
+      set.split(', ').flatMap(cube => {
+        const [value, color] = cube.split(' ')
+
+        v[color] += Number(value)
+      })
+
+      if (v.green > minGreen) minGreen = v.green
+      if (v.blue > minBlue) minBlue = v.blue
+      if (v.red > minRed) minRed = v.red
+    }
+
+    const power = minGreen * minBlue * minRed
+    score += power
+  })
+
+  return score
+}
+
+run({
+  solve,
+  tests: [
+    {
+      input: ``,
+      expected: 2286,
+    },
+  ],
+  onlyTests: true,
+})
