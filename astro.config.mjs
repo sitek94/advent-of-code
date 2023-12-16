fed8b4422c522776c2e2f34225795d2015fe5390
Maciek Sitkowski <msitkowski94@gmail.com>
2023-12-16 15:23:37 +0100

    add deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sitek94.github.io',
+  base: '/advent-of-code',
   srcDir: 'www',
   integrations: [
     react(),
