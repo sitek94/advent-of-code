dbd21daf86c95f1aa673991b1926098410a6e79a
Maciek Sitkowski <msitkowski94@gmail.com>
2024-12-07 08:14:49 +0100

    day 7 part 1

---
+++ puzzles/2024/07/1.ts
@@ -0,0 +1,86 @@
+import {run} from '~/run'
+
+function solve(input: string) {
+  const lines = input.split('\n')
+
+  let score = 0
+
+  for (const line of lines) {
+    const [testValue, ...numbers] = line
+      .split(': ')
+      .map(p => p.split(' '))
+      .flat()
+      .map(Number)
+
+    if (isSomePermutationTruthy(testValue, numbers)) {
+      score += testValue
+      continue
+    }
+  }
+
+  return score
+}
+
+run({
+  solve,
+  tests: [
+    {
+      input: 'test.txt',
+      expected: 100,
+    },
+    {
+      input: 'input.txt',
+      expected: 100,
+    },
+  ],
+})
+
+function isSomePermutationTruthy(testValue: number, numbers: number[]) {
+  const operatorPermutations = allPermutations(['*', '+'], numbers.length - 1)
+
+  for (const operators of operatorPermutations) {
+    if (isTruthyEquation(testValue, numbers, operators)) {
+      return true
+    }
+  }
+  return false
+}
+
+function isTruthyEquation(
+  testValue: number,
+  numbers: number[],
+  operators: string[],
+) {
+  let [total, ...restNumbers] = numbers
+
+  restNumbers.forEach((n, i) => {
+    total = eval(`${total}${operators[i]}${n}`)
+    if (total > testValue) return false
+  })
+
+  if (total === testValue) {
+    return true
+  }
+
+  return false
+}
+
+function allPermutations<T extends string | number>(arr: T[], length: number) {
+  const result = [] as T[][]
+
+  function generate(current: T[]) {
+    if (current.length === length) {
+      result.push([...current])
+      return
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      current.push(arr[i])
+      generate(current)
+      current.pop()
+    }
+  }
+
+  generate([])
+  return result
+}
