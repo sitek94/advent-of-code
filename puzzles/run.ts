f07cc01263bfd5529bb51ce48d84b14ad6fb2d53
Maciek Sitkowski <msitkowski94@gmail.com>
2024-12-09 19:04:21 +0100

    add some warnings in runner

--- puzzles/run.ts
+++ puzzles/run.ts
@@ -26,6 +26,10 @@ export const run = async <TAnswer, TOptions>({
   if (isFinal) {
     tests = tests.filter(test => test.input === 'input.txt')
   }
+  if (!tests.length) {
+    logger.error('No tests found')
+    process.exit(1)
+  }
 
   await runTests({tests, solve})
 }
@@ -70,5 +74,16 @@ const getRunningTime = (fn: () => any) => {
 const readInputFile = async (filename: string) => {
   const pathToExercise = Bun.argv[1].split('/').slice(0, -1).join('/')
   const file = Bun.file(`${pathToExercise}/${filename}`)
-  return file.text()
+  if (!file.exists()) {
+    logger.error(`Input file not found: ${filename}`)
+    process.exit(1)
+  }
+
+  const text = await file.text()
+  if (!text) {
+    logger.error(`Input file empty: ${filename}`)
+    process.exit(1)
+  }
+
+  return text
 }
